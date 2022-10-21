bbc1aa1dd66e4a6902a15c7cf2a92bbeca47f6a7
dikii <dikii@vtb.ru>
2022-10-21 20:37:55 +0300

    Spring constants

---
+++ utility/src/main/java/ru/athena/utility/constant/SpringProfileConstant.java
@@ -0,0 +1,15 @@
+package ru.athena.utility.constant;
+
+/**
+ * Spring profiles constants.
+ */
+public final class SpringProfileConstant {
+
+    private SpringProfileConstant() {
+        throw new UnsupportedOperationException("Operation not supported");
+    }
+
+    public static final String DEV = "dev";
+    public static final String PROD = "prod";
+    public static final String TEST = "test";
+}
