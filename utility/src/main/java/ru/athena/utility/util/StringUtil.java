d5b445fe0dd2642bbd70c20209c0bda22b7cfdec
dikii <dikii@vtb.ru>
2023-01-06 21:31:52 +0300

    Added string util

---
+++ utility/src/main/java/ru/athena/utility/util/StringUtil.java
@@ -0,0 +1,14 @@
+package ru.athena.utility.util;
+
+/**
+ * String utility class
+ */
+public final class StringUtil {
+
+    public static final String EMPTY_STRING = "";
+    public static final String BLANK_STRING = " ";
+
+    private StringUtil() {
+        throw new UnsupportedOperationException("Operation not supported");
+    }
+}
